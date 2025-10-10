314b4ecb1aed7c6190d2f454a899848352a0dde2
swlws <979623074@qq.com>
2025-10-10 11:24:50 +0800

    demo

--- src/views/demo/index.jsx
+++ src/views/demo/index.jsx
@@ -1,3 +1,35 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+let oldAddValueFn;
+let oldCallbackRef;
 export default function Demo() {
-  return <div>Demo</div>;
+  console.log('Demo Render');
+
+  const [value, setValue] = useState(1);
+  const callbackRef = useRef(() => {});
+
+  // 第一次 render 后，值为 true
+  console.log('callbackRef === oldCallbackRef', oldCallbackRef === callbackRef);
+  oldCallbackRef = callbackRef;
+
+  const addValue = useCallback(() => {
+    console.log('call addValue');
+    setValue(value + 1);
+  }, [value]);
+
+  // 因为 value 的变化，导致 addValue 会每次重新创建
+  console.log('addValue === oldAddValueFn', oldAddValueFn === addValue);
+  oldAddValueFn = addValue;
+
+  useEffect(() => {
+    console.log('🎯 useEffect run');
+    return () => console.log('🧹 cleanup');
+  }, []);
+
+  return (
+    <div>
+      value: {value}
+      <button onClick={addValue}>add value</button>
+    </div>
+  );
 }

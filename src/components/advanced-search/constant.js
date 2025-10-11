f15202dafff2aa9c206a10d4659d224b68c9a743
swlws <979623074@qq.com>
2025-10-11 21:19:28 +0800

    operation

--- src/components/advanced-search/constant.js
+++ src/components/advanced-search/constant.js
@@ -7,4 +7,14 @@ export const ENUM_TREE_DATA_OPERATION = {
   SET_TREE_DATA: 'set_tree_data',
   /** 更新目标组件的值 */
   UPDATE_TARGET_COMPONENT_VALUE: 'update_target_component_value',
+  /** 添加树形节点 */
+  ADD_TREE_NODE: 'add_tree_node',
+  /** 删除树形节点 */
+  REMOVE_TREE_NODE: 'remove_tree_node',
+  /** 移动树形节点 */
+  MOVE_TREE_NODE: 'move_tree_node',
+  /** 复制树形节点 */
+  COPY_TREE_NODE: 'copy_tree_node',
+  /** 更新树形节点的状态 */
+  UPDATE_TREE_NODE_STATE: 'update_tree_node_state',
 };

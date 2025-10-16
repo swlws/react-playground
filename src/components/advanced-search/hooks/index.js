97d1d5d36b44fe79230ab15afe27270a6f3a7bfb
swlws <979623074@qq.com>
2025-10-16 19:50:27 +0800

    hoosk

--- src/components/advanced-search/hooks/index.js
+++ src/components/advanced-search/hooks/index.js
@@ -1,3 +1,4 @@
+import { useEffectEvent } from 'react';
 import { useEffect, useState, useSyncExternalStore } from 'react';
 
 /** 强制刷新组件 */
@@ -48,4 +49,82 @@ export function useOnlineStatus() {
     () => navigator.onLine, // 如何在客户端获取值
     () => true // 如何在服务端获取值
   );
+}
+
+/**
+ *  元素淡入
+ * @param {*} ref
+ * @param {*} duration
+ */
+export function useFadeIn(ref, duration) {
+  useEffect(() => {
+    const node = ref.current;
+
+    let startTime = performance.now();
+    let frameId = null;
+
+    function onFrame(now) {
+      const timePassed = now - startTime;
+      const progress = Math.min(timePassed / duration, 1);
+      onProgress(progress);
+      if (progress < 1) {
+        // 我们还有更多的帧需要绘制
+        frameId = requestAnimationFrame(onFrame);
+      }
+    }
+
+    function onProgress(progress) {
+      node.style.opacity = progress;
+    }
+
+    function start() {
+      onProgress(0);
+      startTime = performance.now();
+      frameId = requestAnimationFrame(onFrame);
+    }
+
+    function stop() {
+      cancelAnimationFrame(frameId);
+      startTime = null;
+      frameId = null;
+    }
+
+    start();
+    return () => stop();
+  }, [ref, duration]);
+}
+
+export function useFadeInV2(ref, duration) {
+  const [isRunning, setIsRunning] = useState(true);
+
+  useAnimationLoop(isRunning, (timePassed) => {
+    const progress = Math.min(timePassed / duration, 1);
+    ref.current.style.opacity = progress;
+    if (progress === 1) {
+      setIsRunning(false);
+    }
+  });
+}
+
+function useAnimationLoop(isRunning, drawFrame) {
+  const onFrame = useEffectEvent(drawFrame);
+
+  useEffect(() => {
+    if (!isRunning) {
+      return;
+    }
+
+    const startTime = performance.now();
+    let frameId = null;
+
+    function tick(now) {
+      const timePassed = now - startTime;
+      onFrame(timePassed);
+      frameId = requestAnimationFrame(tick);
+    }
+
+    tick();
+    return () => cancelAnimationFrame(frameId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isRunning]);
 }

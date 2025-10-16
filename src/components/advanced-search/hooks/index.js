aaf6697b0cf4eb24617a62abe4e3f9a851991929
swlws <979623074@qq.com>
2025-10-16 17:53:53 +0800

    hooks

--- src/components/advanced-search/hooks/index.js
+++ src/components/advanced-search/hooks/index.js
@@ -1,7 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState, useSyncExternalStore } from 'react';
 
 /** 强制刷新组件 */
 export function useForceRender() {
   const [, setState] = useState(0);
   return setState;
+}
+
+/** Fetch Data */
+export function useFetchData(url) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          setData(json);
+        }
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, [url]);
+
+  return data;
+}
+
+function onNetworkChange(callback) {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  };
+}
+
+/**
+ * 获取网络状态
+ *    使用  useSyncExternalStore 订阅外部 store
+ * @returns
+ */
+export function useOnlineStatus() {
+  // ✅ 非常好：用内置的 Hook 订阅外部 store
+  return useSyncExternalStore(
+    onNetworkChange, // 只要传递的是同一个函数，React 不会重新订阅
+    () => navigator.onLine, // 如何在客户端获取值
+    () => true // 如何在服务端获取值
+  );
 }

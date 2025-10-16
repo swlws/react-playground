dc1cfbd94241ec910c15d14f27e46b5f3ba87dd5
swlws <979623074@qq.com>
2025-10-16 09:07:40 +0800

    lazy

--- src/define-routes.jsx
+++ src/define-routes.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './views/home/index';
-import Demo from './views/demo/index';
+// import Home from './views/home/index';
+// import Demo from './views/demo/index';
+
+const Home = React.lazy(() => import("./views/home/index"));
+const Demo = React.lazy(() => import("./views/demo/index"));
 
 export default function DefineRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/demo" element={<Demo />} />
-    </Routes>
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
+    </Suspense>
   );
 }

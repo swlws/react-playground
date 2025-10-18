5146341a6a18c5133aa93a48f47cccf4e01f1622
swlws <979623074@qq.com>
2025-10-18 11:09:18 +0800

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/toolkit/index.js';
+
+// 组件
+import App from './App.jsx';
+
+// 样式
 import './style/ui-theme.css';
 import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import store from './store/toolkit/index.js';
-import { Provider } from 'react-redux';
 
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
 //   </StrictMode>
 // );
 

83faa888419893edf8a21363612634a027aff5ab
swlws <979623074@qq.com>
2025-10-26 21:13:04 +0800

    code

--- src/views/G6/core/constants.js
+++ src/views/G6/core/constants.js
@@ -2,37 +2,38 @@
  * 节点状态类型
  */
 export const ENUM_NODE_STATE_TYPE = {
-  HIGHLIGHT: 'highlight',
+  HIGHLIGHT: "highlight",
 };
 /**
  * 边状态类型
  */
 export const ENUM_EDGE_STATE_TYPE = {
-  HIGHLIGHT: 'highlight',
+  HIGHLIGHT: "highlight",
 };
 /**
  * 组合状态类型
  */
 export const ENUM_COMBO_STATE_TYPE = {
-  HIGHLIGHT: 'highlight',
+  HIGHLIGHT: "highlight",
 };
 
 /**
  * 布局算法类型
  */
 export const ENUM_LAYOUT_TYPE = {
-  DEFAULT: 'DEFAULT',
-  FLOW: 'FLOW',
-  ANTV_DAGRE: 'antv-dagre',
-  DENDROGRAM: 'dendrogram',
+  DEFAULT: "DEFAULT",
+  FLOW: "FLOW",
+  ANTV_DAGRE: "antv-dagre",
+  DENDROGRAM: "dendrogram",
+  MINDMAP: "mindmap",
 };
 
 export const ENUM_NODE_SHAPE_TYPE = {
-  CIRCLE: 'circle',
-  RECT: 'rect',
-  HEXAGON: 'hexagon',
-  TRIANGLE: 'triangle',
-  STAR: 'star',
-  DUAL_LABEL_NODE: 'dual-label-node',
-  USER_CARD_NODE: 'user-card-node',
+  CIRCLE: "circle",
+  RECT: "rect",
+  HEXAGON: "hexagon",
+  TRIANGLE: "triangle",
+  STAR: "star",
+  DUAL_LABEL_NODE: "dual-label-node",
+  USER_CARD_NODE: "user-card-node",
 };

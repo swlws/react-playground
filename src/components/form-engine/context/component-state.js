73fcddfcbb480e55e46cbb53de845742e701d37b
swlws <979623074@qq.com>
2025-10-26 18:30:32 +0800

    active

--- src/components/form-engine/context/component-state.js
+++ src/components/form-engine/context/component-state.js
@@ -3,8 +3,15 @@ import { createContext, useContext } from "react";
 
 export const ComponentStateContext = createContext({});
 
+/** 指定组件的状态 */
 export const useComponentState = (componentId) => {
   return useContext(ComponentStateContext)?.[componentId] || {};
+};
+
+/** 获取当前激活的组件状态 */
+export const useActiveComponentState = () => {
+  const state = useContext(ComponentStateContext);
+  return Object.values(state).find((item) => item.active) || {};
 };
 
 export const ComponentStateDispatchContext = createContext({});
